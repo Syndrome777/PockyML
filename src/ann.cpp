2374bafb20f68dbe4cc8f74030243712a362f190
Syndrome777 <issac.sacrifice@gmail.com>
2014-10-08 19:14:25 +0800

    update ann(bp)

--- src/ann.cpp
+++ src/ann.cpp
@@ -5,22 +5,132 @@ ann_model::ann_model()
 {
 }
 
-
-ann_model::ann_model(const mat& in_data)
+ann_model::ann_model(const mat& in_data, const vec& in_data_lab)
 {
-	input_data(in_data);
+	input_data(in_data, in_data_lab);
 }
 
-
-ann_model::~in_data()
-{
-}
-
-void input_data(const mat& input_data)
+ann_model::~ann_model()
 {
 }
 
 
+void ann_model::input_data(const mat& in_data, const vec& in_data_lab)
+{
+	samp_num = in_data.size();
+	dim_num = in_data[0].size();
+	for(int i = 0; i < in_data.size(); i++){
+		vec temp;
+		for(int j = 0; j < in_data[0].size(); j++)
+			temp.push_back(in_data[i][j]);
+		raw_data.push_back(temp);
+		label.push_back(in_data_lab[i]);
+	}
+}
+
+
+void ann_model::init(const int node_n)
+{
+	node_num = node_n;
+
+	map<int,int> label_2_num;//标签转数字，确定class_num，防止label的中断，如label为1，2，4。。。
+	int cnum = 1;
+	for(int i = 0; i < label.size(); i++){
+		if(!label_2_num[label[i]]){
+			label_2_num[label[i]] = cnum;
+			cnum++;
+		}
+	}
+	class_num = cnum - 1;
+
+
+	for(int i = 0; i < dim_num; i++ ){
+		vec temp;
+		for(int j = 0; j < node_num; j++ )
+			temp.push_back(get_rand());
+		W.push_back(temp);
+	}
+	for(int i = 0; i < node_num; i++)
+		W_a.push_back(get_rand());
+
+	for(int i = 0; i < node_num; i++ ){
+		vec temp;
+		for(int j = 0; j < class_num; j++ )
+			temp.push_back(get_rand());
+		V.push_back(temp);
+	}
+	for(int i = 0; i < class_num; i++)
+		V_b.push_back(get_rand());
+
+}
+
+
+
+void ann_model::train(const int node_n)
+{
+}
+
+
+
+vec ann_model::prediction(const vec& in_data)
+{
+	//隐层节点数据
+	vec mid;
+	vec temp;
+	vec resl;
+
+	//输入层到隐层的映射
+	for(int i = 0; i < node_num; i++){
+		for(int j = 0; j < dim_num; j++)
+			temp.push_back(W[j][i]);
+		mid.push_back(multiplication(in_data, temp)- W_a[i]);
+		temp.clear();
+	}
+
+	//隐层到输出层的映射
+	for(int i = 0; i < class_num; i++){
+		for(int j = 0; j < node_num; j++)
+			temp.push_back(V[j][i]);
+		resl.push_back(multiplication(mid, temp) - V_b[i]);
+	}
+
+	return resl;
+}
+
+
+double ann_model::output_error(const vec& ans, const vec& res)
+{
+	double Eor = 0.0;
+	for(int i = 0; i < samp_num; i++){
+		Eor = Eor + 1.0 * (ans[i] - res[i]) * (ans[i] - res[i]);
+	}
+	Eor = Eor / (2 * samp_num);
+	return Eor;
+}
+
+
+
+inline double ann_model::sigmoid(const double x)
+{
+	return 1.0/(1+exp(-0.0001*x));
+}
+
+
+inline double ann_model::get_rand()
+{
+	return 2.0*rand()/RAND_MAX - 1.0;
+}
+
+
+
+inline double multiplication(const vec& vec1, const vec& vec2)
+{
+	double res = 0;
+	for(int i = 0; i < vec1.size(); i++){
+		res = res + vec1[i] * vec2[i];
+	}
+	return res;
+}
 
 
 
--- src/ann_head.h
+++ src/ann_head.h
@@ -10,18 +10,56 @@ using namespace std;
 class ann_model{
 	public:
 		ann_model();
-		ann_model(const mat& in_data);
+		ann_model(const mat& in_data, const vec& in_data_lab);
 		~ann_model();
-		void input_data(const mat& in_data);
-		void train();
+		void init(const int node_n);//初始化层间连接权值和学习速率
+		void input_data(const mat& in_data, const vec& in_data_lab);
+		void train(const int node_n);
 		
 
 	private:
+		//数据参数
 		mat raw_data;//原始数据
+		vec label;//标签数据
 		int samp_num;//样本数目
 		int dim_num;//样本维度
+		int class_num;//类的数目
+	
+		//模型参数
+		double learn_rate;//学习速率
+		int node_num;//隐层节点数
+		int max_iter;//iteration,最大迭代次数
+		
+		mat W;//输入层和隐层间的连接权值，应为dim_num*node_num的矩阵
+		vec W_a;//隐层阀值，长度node_num的向量
+		mat V;//隐层和输出层间的连接权值，应为node_num*class_num的矩阵
+		vec V_b;//输出层阀值，长度为class_num的向量
 
-}
+		double output_error(const vec& ans, const vec& res);//计算输出标签和真实标签的误差，为全局误差　
+		vec prediction(const vec& in_data);//输出层结果预测，基于W和V矩阵
+		vec correction();//参数修正，基于W,V误差修正
+
+		inline double sigmoid(const double x);//activation function，激活函数
+		inline double get_rand();//随机数[-1,1]
+
+};
+
+
+
+inline double multiplication(const vec& vec1, const vec& vec2);
+
+
+/*
+
+算法步骤：
+(1）初始化，随机给定各连接权[w],[v]及阀值θi，rt。  
+(2）由给定的输入输出模式对计算隐层、输出层各单元输出.    
+(3）基于（2）中的输出与真实标签的误差，调整计算新的连接权及阀值. 
+(4）返回第2步，反复训练直到网络设输出误差达到要求结束训练。 
+
+
+*/
+
 
 
 
